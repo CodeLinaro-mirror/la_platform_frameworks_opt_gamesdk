24f544f29752403367f5fa790cca90d7d72525be
Tom Murphy <tomnom@google.com>
2026-09-01 13:59:50 +0000

    test: restore missing #endif in swappy_basic_test
    Restore missing #endif for NATIVE_COVERAGE which was inadvertently
    removed in commit 5432a85b, causing compilation to fail with
    "error: unterminated conditional directive #ifdef NATIVE_COVERAGE".
    
    Bug: 535963917
    Test: ./gradlew :app:externalNativeBuildDebug in test/swappy/testapp
    TAG=agy
    CONV=bfb82828-f08d-4b56-9cc1-8260a98aff0a
    
    Change-Id: I93b83a900d8c8eef6895749b0e48016182695d48

--- test/swappy/swappy_basic_test.cpp
+++ test/swappy/swappy_basic_test.cpp
@@ -53,6 +53,7 @@ public:
 };
 static const ::testing::Environment* const coverage_env =
         ::testing::AddGlobalTestEnvironment(new CoverageEnvironment);
+#endif
 
 namespace swappy {
 class SwappyDisplayManagerJNI {
